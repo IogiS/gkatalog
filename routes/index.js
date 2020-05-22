b529be44b6df4e67c07638579930de949cd5ce6e
IogiS <31819845+IogiS@users.noreply.github.com>
2020-05-22 17:35:45 +0300

    add page 404

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,26 @@ router.get('/', function(req, res, next) {
   res.sendFile('/public/index.html');
 });
 
+router.get('/action', function(req, res, next) {
+  res.render('actiongames');
+  console.log(res.statusCode);
+});
+
+router.get('/game', function(req, res, next) {
+
+  res.render('game');
+});
+
+router.get('/RTY', function(error,req, res ,next){
+  var status =  error.status;
+  res.render('game');
+
+  console.log(res.statusCode);
+  console.log(error.statusCode);
+});
+
+router.use((error, req, res, next) => {
+  console.log('Error status: ', error.status)
+  console.log('Message: ', error.message)
+})
 module.exports = router;
