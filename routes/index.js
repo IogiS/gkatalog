fad4347208e693bc0bac874231a516455111c1a7
IogiS <31819845+IogiS@users.noreply.github.com>
2020-05-25 23:08:04 +0300

    add database access

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,59 @@
 var express = require('express');
 var router = express.Router();
 
+const MongoClient = require("mongodb").MongoClient;
+const url = "mongodb+srv://logisxxx:logisxxx@gkatalogcluster-kazba.mongodb.net/test?retryWrites=true&w=majority";
+const mongoClient = new MongoClient(url, { useNewUrlParser: true });
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.sendFile('/public/index.html');
 });
 
 router.get('/action', function(req, res, next) {
-  res.render('actiongames');
+  res.render('gameslist');
   console.log(res.statusCode);
 });
 
 router.get('/game', function(req, res, next) {
+  mongoClient.connect(function(err, client){
 
-  res.render('game');
+    const db = client.db("gkatalogDB");
+    const collection = db.collection("games");
+
+    //db.games.aggregate([
+    //{ $lookup: { from: "sysreq", localField: "name", foreignField: "name", as: "result" } }])
+
+    if(err) return console.log(err);
+
+    collection.find({name: "Cyberpunk 2077"}).toArray(function(err, results){
+
+      res.render("game", {title: "Cyberpunk 2077" , writes: results });
+      console.log(results);
+
+    });
+  });
+
+
+
 });
 
-router.get('/RTY', function(error,req, res ,next){
-  var status =  error.status;
-  res.render('game');
+router.get('/gamew', function(req, res, next) {
+  try {
+    res.render('222');
+  }
+  catch (e) {
+    res.status(404).send(
 
-  console.log(res.statusCode);
-  console.log(error.statusCode);
+        res.render('refactor')
+    )
+  }
 });
 
 router.use((error, req, res, next) => {
-  console.log('Error status: ', error.status)
+  console.log('Error status: ', res.statusCode)
   console.log('Message: ', error.message)
+  res.render('refactor')
 })
 module.exports = router;
